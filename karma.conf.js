f1daf68322d15cc4492320fa9a17389e8c003582
wlchair <wulei0114@126.com>
2018-06-11 15:21:58 +0800

    over

--- karma.conf.js
+++ karma.conf.js
@@ -1,60 +1,34 @@
-// var webpack = require('webpack'),
-//     unitConfig = require('./webpack.unit');
-// module.exports = function(config) {
-//     config.set({
-//         basePath: '',
-//         frameworks: ['jasmine'],
-//         files: [
-//             'test/test.js'
-//         ],
-//         exclude: ['src/js/lib/*.js'],
-//         preprocessors: {
-//             // 'test/unit/**/*.spec.js': ['webpack']
-//         },
-//         reporters: ['progress'],
-//         port: 9877,
-//         color: true,
-//         logLevel: config.LOG_INFO,
-//         autoWatch: true,
-//         browsers: ['chrome_headless'],
-//         customLaunchers: {
-//             chrome_headless: {
-//                 base: 'Chrome',
-//                 flags: [
-//                     '--headless',
-//                     '--disable-gpu',
-//                     '--remote-debugging-port=9222'
-//                 ]
-//             }
-//         },
-//         singleRun: false,
-//         concurrency: Infinity,
-//         webpack: unitConfig,
-//         coverageIstanbulReporter: {
-//             reports: ['text-summary', 'html'],
-//             fixWebpackSourcePaths: true
-//         },
-//         // browserDisconnectTimeout: 20000,
-//         // browserNoActivityTimeout: 20000,
-//         browserConsoleLogOptions: {
-//             level: 'log',
-//             terminal: true
-//         },
-//     })
-// }
-
-
-
+var webpack = require('webpack'),
+    unitConfig = require('./webpack.unit');
 module.exports = function(config) {
     config.set({
+        basePath: '',
         frameworks: ['jasmine'],
-        files: ['test/test.js'],
-        browsers: ["ChromeHeadless"],
-        browserConsoleLogOptions: {
-            level: 'log'
+        files: [
+            'test/test.js'
+        ],
+        exclude: ['src/js/lib/*.js'],
+        preprocessors: {
+            // 'test/unit/**/*.spec.js': ['webpack']
         },
         reporters: ['progress'],
-        autoWatch: false,
-        singleRun: true
+        port: 9877,
+        color: true,
+        logLevel: config.LOG_INFO,
+        autoWatch: true,
+        browsers: ['ChromeHeadless'],
+        singleRun: false,
+        concurrency: Infinity,
+        webpack: unitConfig,
+        coverageIstanbulReporter: {
+            reports: ['text-summary', 'html'],
+            fixWebpackSourcePaths: true
+        },
+        // browserDisconnectTimeout: 20000,
+        // browserNoActivityTimeout: 20000,
+        browserConsoleLogOptions: {
+            level: 'log',
+            terminal: true
+        },
     })
 }
